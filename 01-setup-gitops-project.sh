fad70838dd097a174c8379fd6799d8d9ead0b7b4
mmartofel <mmartofe@redhat.com>
2022-11-26 01:36:12 +0100

    typo at applications directory name

--- 01-setup-gitops-project.sh
+++ 01-setup-gitops-project.sh
@@ -1,5 +1,5 @@
 oc apply -f ./application-project/appproject-requirements.yaml -n openshift-gitops
-oc apply -f ./application/application-operators.yaml -n openshift-gitops
-oc apply -f ./application/application-project-bookinfo.yaml -n openshift-gitops
-oc apply -f ./application/application-project-dev.yaml -n openshift-gitops
-oc apply -f ./application/application-project-istio-system.yaml -n openshift-gitops
+oc apply -f ./applications/application-operators.yaml -n openshift-gitops
+oc apply -f ./applications/application-project-bookinfo.yaml -n openshift-gitops
+oc apply -f ./applications/application-project-dev.yaml -n openshift-gitops
+oc apply -f ./applications/application-project-istio-system.yaml -n openshift-gitops
