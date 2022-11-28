0151568141cbcb5dad9b464dcd94d12cfb57be6a
mmartofel <mmartofe@redhat.com>
2022-11-28 18:01:50 +0100

    structure reaorg due to namespace creation errors

--- 01-setup-gitops-project.sh
+++ 01-setup-gitops-project.sh
@@ -1,5 +1,5 @@
 oc apply -f ./application-project/appproject-requirements.yaml -n openshift-gitops
 oc apply -f ./applications/application-operators.yaml -n openshift-gitops
-oc apply -f ./applications/application-project-bookinfo.yaml -n openshift-gitops
+oc apply -f ./applications/application-project-openshift-projects.yaml -n openshift-gitops
 oc apply -f ./applications/application-project-dev.yaml -n openshift-gitops
 oc apply -f ./applications/application-project-istio-system.yaml -n openshift-gitops
